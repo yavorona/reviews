e12435ff091de49a9ff8135617a152d6aff0534c
Polina Nguen <p@polinan.com>
2020-01-27 12:28:47 -0800

    Create tabs with css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,12 +3,44 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      view: "reviews",
+      reviews: []
+    };
+  }
+  changeView(option) {
+    this.setState({
+      view: option
+    });
   }
 
   render() {
     return (
-      <div>
-        <h1>Trippy Reviews</h1>
+      <div className="nav">
+        <span
+          className={
+            this.state.view === "reviews" ? "nav-selected" : "nav-unselected"
+          }
+          onClick={() => this.changeView("reviews")}
+        >
+          Reviews
+        </span>
+        <span
+          className={
+            this.state.view === "q&a" ? "nav-selected" : "nav-unselected"
+          }
+          onClick={() => this.changeView("q&a")}
+        >
+          Q+A
+        </span>
+        <span
+          className={
+            this.state.view === "room_tips" ? "nav-selected" : "nav-unselected"
+          }
+          onClick={() => this.changeView("room_tips")}
+        >
+          Room tips
+        </span>
       </div>
     );
   }
