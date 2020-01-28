ecd52a81f3eed34a94a794cd2581cce9b3755d49
Polina Nguen <p@polinan.com>
2020-01-28 14:17:32 -0800

    Add reviews header

--- client/src/components/ReviewsControls.jsx
+++ client/src/components/ReviewsControls.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import {
+  DropdownButton,
+  Dropdown,
+  DropdownMenu,
+  DropdownToggle,
+  DropdownItem
+} from "reactstrap";
 
 class ReviewsControls extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render() {
-    return <div>Review Controls</div>;
+    return (
+      <div className="reviewControlTitle">
+        <div className="reviewHeader">
+          <div className="headerDivider">
+            <h2 className="reviewHeaderText">Reviews</h2>
+          </div>
+          <div>
+            <button className="writeReviewButton">Write a Review</button>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
+    );
   }
 }
 
